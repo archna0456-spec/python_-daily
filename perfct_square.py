dd88d50c5c4e32a05af694892c97fcb61d11d121
Archna <archna0456@gmail.com>
2026-05-08 22:51:50 +0530

    perfect square

---
+++ perfct_square.py
@@ -0,0 +1,13 @@
+import math
+
+
+def checkperfectsquare(x):
+    if (math.ceil(math.sqrt(n)) ==
+            math.floor(math.sqrt(n))):
+        print("True")
+    else:
+        print("False")
+
+
+n = 49
+checkperfectsquare(n)
